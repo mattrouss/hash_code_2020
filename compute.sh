3b466d3933dd04fe6741372a0b44f03ae9a16cb1
Matthew Rousseau <matthew.rousseau@epita.fr>
2020-02-20 21:08:43 +0100

    Add script to compute all input files

---
+++ compute.sh
@@ -0,0 +1,6 @@
+cat a | ./hash_code_2020 > a.out
+cat b | ./hash_code_2020 > b.out
+cat c | ./hash_code_2020 > c.out
+cat d | ./hash_code_2020 > d.out
+cat e | ./hash_code_2020 > e.out
+cat f | ./hash_code_2020 > f.out
